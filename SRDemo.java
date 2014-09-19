f89b9d8c27b5a1035ce7d220361adb1c08ce9e89
dagberg <dagberg@users.noreply.github.com>
2014-09-18 20:19:54 -0500

    Create SRDemo.java

---
+++ SRDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.StudentRecord;
+public class SRDemo
+{
+
+   public static void main(String [] args)
+   {
+      StudentRecord Rudy = new StudentRecord();
+      Rudy.readInput();
+      Rudy.calcGrade(quiz1, quiz2, midterm, finalexam);
+      Rudy.letter(grade);
+      Rudy.writeOutput();
+   }
+   
+}
